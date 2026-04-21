d5d6f731859c27561b2ce65f1025c02ee03aa47c
Yevhen Malyshev <meugen.82@gmail.com>
2026-04-21 23:52:03 +0300

    Refactored integration tests to use TestContainers

--- src/test/java/com/ua/teamconnect/tracker/controllers/StackControllerTest.java
+++ src/test/java/com/ua/teamconnect/tracker/controllers/StackControllerTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
 
 import static com.ua.teamconnect.tracker.util.TestUtil.buildClient;
 
@@ -24,20 +27,35 @@ class StackControllerTest {
         stackRepository.deleteAll();
     }
 
+    private Stack newStack(String name) {
+        var stack = new Stack();
+        stack.setName(name);
+        return stack;
+    }
+
+    private void validateStackItem(WebTestClient.BodyContentSpec spec, int index, String name) {
+        var prefix = String.format("$[%d]", index);
+        spec.jsonPath(prefix + ".id").isNumber()
+            .jsonPath(prefix + ".name").isEqualTo(name);
+    }
+
     @Test
     void findAll_stacksExists_isOk() {
-        var javaStack = new Stack();
-        javaStack.setName("Java");
-        stackRepository.save(javaStack);
+        stackRepository.saveAll(List.of(
+            newStack("Java"), newStack("Python"),
+            newStack("Ruby"), newStack("JavaScript")
+        ));
 
-        buildClient(port).get()
+        var spec = buildClient(port).get()
             .uri("/stacks")
             .exchange()
             .expectStatus().isOk()
             .expectBody()
             .jsonPath("$").isArray()
-            .jsonPath("$.length()").isEqualTo(1)
-            .jsonPath("$[0].id").isNumber()
-            .jsonPath("$[0].name").isEqualTo("Java");
+            .jsonPath("$.length()").isEqualTo(4);
+        validateStackItem(spec, 0, "Java");
+        validateStackItem(spec, 1, "Python");
+        validateStackItem(spec, 2, "Ruby");
+        validateStackItem(spec, 3, "JavaScript");
     }
 }
