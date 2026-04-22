4ceef6d033a7f0ff1a140dcc59942c0046486f9a
Yevhen Malyshev <meugen.82@gmail.com>
2026-04-22 15:42:02 +0300

    Added not null for Stack.name

--- src/main/java/com/ua/teamconnect/tracker/model/Stack.java
+++ src/main/java/com/ua/teamconnect/tracker/model/Stack.java
@@ -9,7 +9,7 @@ public class Stack {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String name;
 
     public Long getId() {
