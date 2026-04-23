98d1a6aafabe3603035feead5e1bb13dd1ab751a
Yevhen Malyshev <meugen.82@gmail.com>
2026-04-23 17:15:17 +0300

    Added schema definition for stack

--- src/main/java/com/ua/teamconnect/tracker/dto/StackDto.java
+++ src/main/java/com/ua/teamconnect/tracker/dto/StackDto.java
@@ -2,6 +2,7 @@ package com.ua.teamconnect.tracker.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(name = "Stack", description = "Data of the stack")
 public record StackDto(
     @Schema(description = "Id of the stack", example = "1")
     Long id,
