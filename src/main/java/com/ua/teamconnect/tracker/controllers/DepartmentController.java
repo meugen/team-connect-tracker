20343ef75a89ff28dae6b061e576579f8dcb5d20
Yevhen Malyshev <meugen.82@gmail.com>
2026-04-23 11:13:44 +0300

    Added Swagger configuration

--- src/main/java/com/ua/teamconnect/tracker/controllers/DepartmentController.java
+++ src/main/java/com/ua/teamconnect/tracker/controllers/DepartmentController.java
@@ -2,6 +2,7 @@ package com.ua.teamconnect.tracker.controllers;
 
 import com.ua.teamconnect.tracker.dto.DepartmentDto;
 import com.ua.teamconnect.tracker.services.DepartmentService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/departments", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Department Controller", description = "Endpoints related to departments")
 public class DepartmentController {
 
     private final DepartmentService departmentService;
--- src/main/java/com/ua/teamconnect/tracker/dto/DepartmentDto.java
+++ src/main/java/com/ua/teamconnect/tracker/dto/DepartmentDto.java
@@ -1,6 +1,16 @@
 package com.ua.teamconnect.tracker.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record DepartmentDto(Long id, String name, @JsonProperty("head_id") Long headId) {
+@Schema(name = "Department", description = "Data of the department")
+public record DepartmentDto(
+    @Schema(description = "Id of the department", example = "1")
+    Long id,
+    @Schema(description = "Name of the department", example = "Software Development")
+    String name,
+    @Schema(description = "Head Id of this department", example = "1")
+    @JsonProperty("head_id")
+    Long headId
+) {
 }
