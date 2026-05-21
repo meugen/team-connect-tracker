f275c879f5b488f3bd8768e515765bade8ec4e49
Yevhen Malyshev <meugen.82@gmail.com>
2026-05-21 10:15:07 +0300

    Updated Swagger doc for the new endpoint

--- src/main/java/com/ua/teamconnect/tracker/controller/FilesController.java
+++ src/main/java/com/ua/teamconnect/tracker/controller/FilesController.java
@@ -1,7 +1,12 @@
 package com.ua.teamconnect.tracker.controller;
 
+import com.ua.teamconnect.tracker.model.annotation.ApiResponseBadRequest;
+import com.ua.teamconnect.tracker.model.annotation.ApiResponseOk;
+import com.ua.teamconnect.tracker.model.annotation.ApiResponseUnauthorized;
 import com.ua.teamconnect.tracker.model.dto.UploadedFileDto;
 import com.ua.teamconnect.tracker.service.FilesService;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,13 +20,17 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/files")
 @RequiredArgsConstructor
+@ApiResponseOk @ApiResponseUnauthorized
+@Tag(name = "Files Controller", description = "Endpoints related to file management")
 public class FilesController {
 
     private final FilesService filesService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ApiResponseBadRequest
     public UploadedFileDto uploadFile(
         @AuthenticationPrincipal Jwt jwt,
+        @Parameter(description = "File to upload", required = true)
         @RequestParam MultipartFile file
     ) {
         return filesService.uploadFile(jwt.getSubject(), file);
