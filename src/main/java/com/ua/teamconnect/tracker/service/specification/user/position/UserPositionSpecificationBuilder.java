3af071f4111af6ab2b172828311f614b7faa411b
Petro Matsenko <ebatgibshik@gmail.com>
2026-05-22 10:14:14 +0300

    Support first and last name search in user filtering

--- src/main/java/com/ua/teamconnect/tracker/service/specification/user/position/UserPositionSpecificationBuilder.java
+++ src/main/java/com/ua/teamconnect/tracker/service/specification/user/position/UserPositionSpecificationBuilder.java
@@ -1,6 +1,7 @@
 package com.ua.teamconnect.tracker.service.specification.user.position;
 
 import com.ua.teamconnect.tracker.model.entity.UserPosition;
+import com.ua.teamconnect.tracker.model.exception.UnsupportedFilterException;
 import com.ua.teamconnect.tracker.service.specification.common.FetchSpecification;
 import com.ua.teamconnect.tracker.util.Pair;
 import org.springframework.data.jpa.domain.Specification;
@@ -17,6 +18,16 @@ public class UserPositionSpecificationBuilder {
     static final String PARAM_DEPARTMENT = "department";
     static final String PARAM_POSITION = "position";
     static final String PARAM_STACK = "stack";
+    private static final Set<String> SUPPORTED_PARAMS = Set.of(
+                    PARAM_SEARCH,
+                    PARAM_DEPARTMENT,
+                    PARAM_POSITION,
+                    PARAM_STACK,
+                    "page",
+                    "size",
+                    "sort",
+                    "order"
+                );
 
     /**
      * first - specification for data
@@ -25,6 +36,7 @@ public class UserPositionSpecificationBuilder {
     public Pair<Specification<UserPosition>, Specification<UserPosition>> build(
         Map<String, String> params, Set<String> fetchFields
     ) {
+        validateSupportedParams(params);
         var specification = Specification.<UserPosition>unrestricted();
         specification = SearchUserPositionSpecification.wrap(specification, params.get(PARAM_SEARCH));
         specification = DepartmentIdUserPositionSpecification.wrap(specification, params.get(PARAM_DEPARTMENT));
@@ -39,5 +51,13 @@ public class UserPositionSpecificationBuilder {
 
     public Pair<Specification<UserPosition>, Specification<UserPosition>> build(Map<String, String> params) {
         return build(params, Set.of("user", "position.department"));
+    }
+    
+    private void validateSupportedParams(Map<String, String> params) {
+        params.keySet().forEach(param -> {
+            if (!SUPPORTED_PARAMS.contains(param)) {
+                throw new UnsupportedFilterException(param);
+            }
+        });
     }
 }
