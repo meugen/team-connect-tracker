e016e7cf0858516c2c82a3ec25a8d653d402900a
Yevhen Malyshev <meugen.82@gmail.com>
2026-05-28 19:30:04 +0300

    Added swagger docs

--- src/main/java/com/ua/teamconnect/tracker/controller/HolidayController.java
+++ src/main/java/com/ua/teamconnect/tracker/controller/HolidayController.java
@@ -1,7 +1,10 @@
 package com.ua.teamconnect.tracker.controller;
 
+import com.ua.teamconnect.tracker.model.annotation.ApiResponseOk;
+import com.ua.teamconnect.tracker.model.annotation.ApiResponseUnauthorized;
 import com.ua.teamconnect.tracker.model.dto.HolidayDto;
 import com.ua.teamconnect.tracker.service.HolidayService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +15,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/holidays")
 @RequiredArgsConstructor
+@Tag(name = "Holiday Controller", description = "Endpoints for managing holidays")
+@ApiResponseOk
+@ApiResponseUnauthorized
 public class HolidayController {
 
     private final HolidayService holidayService;
 
     @GetMapping("/upcoming")
+    @Tag(name = "Find Upcoming Holidays", description = "Get a list of upcoming holidays")
     public List<HolidayDto> findUpcoming() {
         return holidayService.findUpcoming();
     }
